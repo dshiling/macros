aa9a5b3da4fa28ad164d995f3e1c25070767b1b7
Daniel Shiling <dshiling@squarespace.com>
2018-03-06 12:26:43 -0500

    Fix Init

--- macros/__init__.py
+++ macros/__init__.py
@@ -5,40 +5,13 @@ macros
 Python app for finding and tracking common T&S macros
 """
 import os
-from dromedary import load_configuration
-
-from flask_maker import create_app
-import logging.config
+from flask import Flask
 
 __version__ = '0.1.0'
-__author__ = 'T&S'
-__email__ = 'trustandsafety-all@squarespace.com'
-__description__ = 'Macros application for t&s language snippets'
+__author__ = 'dshiling'
+__description__ = 'Macros application for common language snippets'
 
-
-ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
-PROJECT_DIR = os.path.dirname(ROOT_DIR)
-
-
-config = load_configuration(
-    PROJECT_DIR,
-    base_config_file='config.yaml',
-    local_config_file='config.local.yaml',
-    use_env_overrides=True
-)
-
-
-def setup_logging(logging_config):
-    if logging_config:
-        logging.config.dictConfig(logging_config)
-    else:
-        logging.basicConfig(level='WARNING')
-
-
-setup_logging(config.get('LOGGING'))
-
-
-app = create_app(__name__, config)
+app = Flask(__name__)
 
 
 from macros import views
